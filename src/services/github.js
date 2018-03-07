f51269e75cd973ac2a3a70edf604e97f680535c0
Ivan Nejezchleb <ivan.nejezchleb@gooddata.com>
2018-03-07 15:32:49 +0700

    * fix react warning and better error messages

--- src/services/github.js
+++ src/services/github.js
@@ -1,7 +1,7 @@
 import 'isomorphic-fetch';
 
-export function fetchIssues(/* username, repository */) {
-    return Promise.resolve([{ title: 'Foo', body: 'foooooooo' }]);
+export function fetchIssues(/* username, repository*/) {
+    return Promise.resolve([{ title: 'Foo', body: 'foooooooo', id: 1 }]);
 }
 
 const REPOSITORY = {
@@ -24,7 +24,7 @@ const REPOSITORY = {
 
 export function fetchRepository(username, repository) {
     if (!REPOSITORY[username]) {
-        return Promise.reject({ code: 404, message: `Not found ${username}:${repository}` });
+        return Promise.reject({ code: 404, message: `Unknown Github user: ${username}` });
     }
 
     const repo = REPOSITORY[username][repository];
@@ -33,5 +33,5 @@ export function fetchRepository(username, repository) {
         return Promise.resolve(repo);
     }
 
-    return Promise.reject({ code: 404 });
+    return Promise.reject({ code: 404, message: `Repository: ${repository} not found` });
 }
